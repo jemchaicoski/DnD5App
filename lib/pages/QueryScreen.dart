58751d2a67004a4bbff66b87fb51cb6bf19816e2
jemchaicoski <57416346+jemchaicoski@users.noreply.github.com>
2022-10-18 16:46:11 -0300

    Example query working

--- lib/pages/QueryScreen.dart
+++ lib/pages/QueryScreen.dart
@@ -1,14 +1,34 @@
+import 'package:dnd5_app/services/DndService.dart';
 import 'package:flutter/material.dart';
 
-class QueryScreen extends StatelessWidget {
+class QueryScreen extends StatefulWidget{
   final String title;
   const QueryScreen(this.title);
+
+  @override
+  State<QueryScreen> createState() => _QueryScreenState();
+}
+
+class _QueryScreenState extends State<QueryScreen> {
+  DndService dndService = DndService();
+
+  @override
+  void initState() {
+    _getThingsOnStartup().then((value){
+      print('Async done');
+    });
+    super.initState();
+  }
+
+  Future _getThingsOnStartup() async {
+    await dndService.getAllSpells();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: ListView.builder(
         itemCount: 10,
@@ -19,6 +39,5 @@ class QueryScreen extends StatelessWidget {
         },
       ),
     );
-    ;
   }
 }
