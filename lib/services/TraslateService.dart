eccb6aa902927a6d03d1796a803d6bd08124b009
jemchaicoski <jemchaicoski@hotmail.com>
2022-12-06 22:33:47 -0300

    description, name and higher level translated

--- lib/services/TraslateService.dart
+++
@@ -1,9 +0,0 @@
-import 'package:translator/translator.dart';
-
-class TranslatorService{
-  GoogleTranslator translator = new GoogleTranslator();
-
-  translateTextEnToPt(String text){
-    return translator.translate(text, from: 'en', to: 'pt');
-  }
-}
